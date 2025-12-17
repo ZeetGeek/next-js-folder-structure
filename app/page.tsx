b395a80ef697e85835a209aabb9375ade30e856d
ZeetGeek <zeetgeek435@gmail.com>
2025-12-17 15:19:01 +0530

    update package-lock.json to add husky dependency for pre-commit hooks; modify globals.css to change text color to white; adjust page.tsx layout by removing unnecessary box element and refining button styles

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import Image from "next/image";
 export default function Home() {
     return (
         <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-            <div className="box"></div>
             <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between bg-white px-16 py-32 sm:items-start dark:bg-black">
                 <Image alt="Next.js logo" className="dark:invert" height={20} priority src="/next.svg" width={100} />
                 <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
@@ -39,7 +38,7 @@ export default function Home() {
                         Deploy Now
                     </a>
                     <a
-                        className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] md:w-[158px] dark:border-white/[.145] dark:hover:bg-[#1a1a1a]"
+                        className="mt-[20px] flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] md:w-[158px] dark:border-white/[.145] dark:hover:bg-[#1a1a1a]"
                         href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
                         rel="noopener noreferrer"
                         target="_blank"

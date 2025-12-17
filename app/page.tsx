f7088c193cf21beaee2dfc73da630f38c7e08e8a
ZeetGeek <zeetgeek435@gmail.com>
2025-12-17 12:06:54 +0530

    refactor stylelint configuration to enhance CSS linting rules; remove unused styles from globals.css; update page structure by adding a new box element

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
 import Image from "next/image";
 
 export default function Home() {
-    console.log("Hello World");
-
     return (
         <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+            <div className="box"></div>
             <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between bg-white px-16 py-32 sm:items-start dark:bg-black">
                 <Image alt="Next.js logo" className="dark:invert" height={20} priority src="/next.svg" width={100} />
                 <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">

a082b26a03be5b11f599603a482fdceb7acbc62e
ZeetGeek <zeetgeek435@gmail.com>
2025-12-18 11:23:48 +0530

    chore: add console log for testing purposes

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@ import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 
 function Page() {
+    // eslint-disable-next-line no-console
+    console.log("test");
+
     return (
         <div className="bg-neutral-900 font-bold shadow-sm shrink text-red-500">
             <Button className={cn(`rounded-sm underline`)}></Button>

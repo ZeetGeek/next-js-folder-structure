d2a718a539ca352e28154d8349bf34ab3433151e
ZeetGeek <zeetgeek435@gmail.com>
2025-12-18 11:25:13 +0530

    chore: remove console log used for testing

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,6 @@ import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 
 function Page() {
-    // eslint-disable-next-line no-console
-    console.log("test");
-
     return (
         <div className="bg-neutral-900 font-bold shadow-sm shrink text-red-500">
             <Button className={cn(`rounded-sm underline`)}></Button>

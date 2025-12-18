c11fa1f954718ae7d7f8ef6f0d09eee094db8963
ZeetGeek <zeetgeek435@gmail.com>
2025-12-18 14:34:14 +0530

    feat: add Playwright configuration and initial tests

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 function Home() {
-    return <div>Home</div>;
+    return <div className={`bg-green-500 capitalize hover:bg-red-500 hover:text-white p-5 text-black`}>Hello Word</div>;
 }
 
 export default Home;

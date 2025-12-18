a73dc4873ca88813b77239fc7bc0a3d1becb6488
ZeetGeek <zeetgeek435@gmail.com>
2025-12-18 12:20:28 +0530

    feat: add Safari detection and apply conditional styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,5 @@
-import { Button } from "@/components/ui/button";
-import { cn } from "@/lib/utils";
-
-function Page() {
-    return (
-        <div className="bg-neutral-900 font-bold shadow-sm shrink text-red-500">
-            <Button className={cn(`rounded-sm underline`)}></Button>
-        </div>
-    );
+function Home() {
+    return <div>Home</div>;
 }
 
-export default Page;
+export default Home;

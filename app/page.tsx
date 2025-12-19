e06ca52ca1e411999d0001a5fcfb1b60f11fd327
ZeetGeek <zeetgeek435@gmail.com>
2025-12-19 15:49:37 +0530

    fix: update Image component properties for consistency and performance

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,7 @@ function Home() {
     return (
         <main>
             <div className={`bg-green-500 capitalize hover:bg-red-500 hover:text-white p-5 text-black`}>Hello Word</div>
-            <Image
-                alt="Mountain Landscape"
-                className="object-cover size-200"
-                height={2048}
-                quality={70}
-                src="/mountains.jpg"
-                width={3089}
-            />
+            <Image alt="Mountain Landscape" height={530} quality={70} src="/mountains.jpg" width={800} />
         </main>
     );
 }

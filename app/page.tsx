569d2d555b774524c02bf0b727580e478c70ef8b
ZeetGeek <zeetgeek435@gmail.com>
2025-12-19 11:08:54 +0530

    Added a main tag wrapper

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 function Home() {
-    return <div className={`bg-green-500 capitalize hover:bg-red-500 hover:text-white p-5 text-black`}>Hello Word</div>;
+    return (
+        <main>
+            <div className={`bg-green-500 capitalize hover:bg-red-500 hover:text-white p-5 text-black`}>Hello Word</div>
+        </main>
+    );
 }
 
 export default Home;

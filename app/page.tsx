71994a6347ab5fecab0cbf64d000e7d704b07985
ZeetGeek <zeetgeek435@gmail.com>
2025-12-19 14:45:48 +0530

    Refactor code structure for improved readability and maintainability

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
+import Image from "next/image";
+
 function Home() {
     return (
         <main>
             <div className={`bg-green-500 capitalize hover:bg-red-500 hover:text-white p-5 text-black`}>Hello Word</div>
+            <Image
+                alt="Mountain Landscape"
+                className="object-cover size-200"
+                height={2048}
+                quality={70}
+                src="/mountains.jpg"
+                width={3089}
+            />
         </main>
     );
 }

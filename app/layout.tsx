559f8620c60c6fc7d0e88b93e86c7c5ea731ddaf
ZeetGeek <zeetgeek435@gmail.com>
2025-12-23 22:07:55 +0530

    chore: update dependencies and remove unused assets
    - Updated dependencies in package.json:
      - Added @tanstack/react-query and axios.
      - Updated lucide-react, next, react, react-dom, react-hook-form, recharts, and zod to their latest versions.
      - Updated @types/node and eslint-config-next.
      - Updated eslint-plugin-perfectionist.
    
    - Removed unused image and SVG files from the public directory:
      - achromatic.jpg
      - file.svg
      - globe.svg
      - mountains.jpg
      - next.svg
      - vercel.svg
      - window.svg

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 
-import LayoutWrapper from "@/components/layout-wrapper";
-
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,9 +25,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-                <LayoutWrapper>{children}</LayoutWrapper>
-            </body>
+            <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black`}>{children}</body>
         </html>
     );
 }

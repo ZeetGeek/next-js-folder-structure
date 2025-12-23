5f50c595039899c491a7a47cf2d28e63623def34
ZeetGeek <zeetgeek435@gmail.com>
2025-12-23 22:25:18 +0530

    chore: update dependencies and add postcss configuration
    - Added autoprefixer, next-auth, postcss, postcss-preset-env, and zustand to dependencies.
    - Updated browserslist configuration in package.json.
    - Enhanced postcss.config.mjs to include postcss-preset-env with custom settings.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 function Home() {
-    return <div>Home</div>;
+    return <div className="bg-white flex p-5 test-prefix">Home</div>;
 }
 
 export default Home;
--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,6 +1,13 @@
 const config = {
     plugins: {
         "@tailwindcss/postcss": {},
+        "postcss-preset-env": {
+            autoprefixer: {},
+            stage: 3,
+            features: {
+                "custom-properties": false,
+            },
+        },
     },
 };
 

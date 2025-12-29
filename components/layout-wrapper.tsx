9b61ebacef8f564125d64adfd4badb9cf2ac42a0
ZeetGeek <zeetgeek435@gmail.com>
2025-12-29 14:32:44 +0530

    removed old code

--- components/layout-wrapper.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function LayoutWrapper({ children }: { children: React.ReactNode }) {
-    return <>{children}</>;
-}
-
-export default LayoutWrapper;
--- components/ui/skeleton.tsx
+++
@@ -1,7 +0,0 @@
-import { cn } from "@/lib/utils";
-
-function Skeleton({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
-    return <div className={cn("animate-pulse bg-muted rounded-md", className)} {...props} />;
-}
-
-export { Skeleton };
